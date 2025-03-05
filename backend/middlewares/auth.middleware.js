973bd5998ca85d5282fce158835f02c981b61d67
ravirajutture1011 <ravirajutture1026@gmail.com>
2025-03-05 16:13:25 +0530

    middleware is completed

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,12 @@
+
+export const protectRoute = async (req, res, next) => {
+    const accessToken = req.cookies.accessToken;
+
+    if (!accessToken) {
+        return res.status(401).json({ message: "Access token missing" });
+    }
+
+    // Attach token to request for next middleware
+    req.accessToken = accessToken;
+    next();
+};
